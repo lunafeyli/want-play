62932b14c9cda969457abb2221ec5fee695033a6
pedrohsfontoura <pedrohenrisantosfontoura@gmail.com>
2022-05-10 20:12:04 -0300

    :lipstick: add Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,53 @@
 import type { NextPage } from "next";
+import { Logo } from "~/assets/logo";
+import {
+	GithubLogo as GitHubIcon,
+	Question as AboutIcon,
+} from "phosphor-react";
+import { styled } from "@styles";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-	return <></>;
+	const Header = styled("header", {
+		width: "100vw",
+		padding: "32px 48px",
+		$flex: {
+			dir: "row",
+			justify: "space-between",
+			align: "center",
+		},
+
+		svg: {
+			color: "$white",
+			height: 32,
+
+			"&.logo": {
+				height: 64,
+			},
+		},
+	});
+
+	return (
+		<>
+			<Header>
+				<Link
+					href={"https://github.com/pedrohsfontoura/want-play"}
+					type="_blank"
+				>
+					<a>
+						<GitHubIcon
+							style={{ height: 24, width: 24 }}
+							weight="fill"
+						/>
+					</a>
+				</Link>
+				<h1>
+					<Logo />
+				</h1>
+				<AboutIcon style={{ height: 24, width: 24 }} weight="fill" />
+			</Header>
+		</>
+	);
 };
 
 export default Home;
--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,13 @@
 import { createStitches } from "@stitches/react";
+import { Property } from "@stitches/react/types/css";
+
+type Flex = {
+	dir?: Property.FlexDirection;
+	align?: Property.AlignItems;
+	justify?: Property.JustifyContent;
+	wrap?: Property.FlexWrap;
+	fullCenter?: boolean;
+};
 
 export const { styled, globalCss } = createStitches({
 	theme: {
@@ -9,6 +18,21 @@ export const { styled, globalCss } = createStitches({
 			main500: "#463E51",
 			main600: "#2C2735",
 		},
+	},
+	utils: {
+		$flex: ({
+			dir = "column",
+			align = "flex-start",
+			justify = "flex-start",
+			wrap = "nowrap",
+			fullCenter,
+		}: Flex) => ({
+			display: "flex",
+			alignItems: fullCenter ? "center" : align,
+			justifyContent: fullCenter ? "center" : justify,
+			flexWrap: wrap,
+			flexDirection: dir,
+		}),
 	},
 });
 
